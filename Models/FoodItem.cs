ee2a466617d56dfedcf9d164c5e8e52596ee1b2c
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-24 22:42:09 -0700

    Gen webapp. Add FoodCategory and FoodItem.

---
+++ Models/FoodItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionTrackerRazorPages.Models
+{
+    public class FoodItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int FoodCategoryId { get; set; }
+        public FoodCategory FoodCategory { get; set; } // navigation property
+
+        public decimal ServingSize { get; set; }
+
+        public decimal Calories { get; set; }
+        public decimal Fat { get; set; }
+    }
+}
