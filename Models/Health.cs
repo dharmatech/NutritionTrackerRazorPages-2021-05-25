8bd504cdb7c4bfe46bc40fbf75a38272afc8feb4
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-25 00:35:40 -0700

    Add Models\Health

---
+++ Models/Health.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionTrackerRazorPages.Models
+{
+    public class Health
+    {
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; }
+
+        public int FoodItemId { get; set; }
+
+        public FoodItem FoodItem { get; set; } // navigation
+
+        public decimal Amount { get; set; }
+
+        public decimal Calories => FoodItem.Calories * Amount;
+    }
+}
