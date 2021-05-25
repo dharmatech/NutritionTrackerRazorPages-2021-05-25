72688a79929b20d36304624dba8a5b8f6958cbc9
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-24 22:37:49 -0700

    notes.ps1

---
+++ notes.ps1
@@ -0,0 +1,47 @@
+
+cd ~/Dropbox/Documents/VisualStudio
+
+mkdir NutritionTrackerRazorPages
+
+cd NutritionTrackerRazorPages
+
+# ----------------------------------------------------------------------
+
+git init
+
+# ----------------------------------------------------------------------
+
+dotnet new gitignore
+
+# ----------------------------------------------------------------------
+
+dotnet new webapp
+
+# ----------------------------------------------------------------------
+
+dotnet add package Microsoft.EntityFrameworkCore.SQLite -v 5.0.0-*
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 5.0.0-*
+dotnet add package Microsoft.EntityFrameworkCore.Design -v 5.0.0-*
+dotnet add package Microsoft.EntityFrameworkCore.Tools -v 5.0.0-*
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design -v 5.0.0-*
+dotnet add package Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore -v 5.0.0-*  
+
+# ----------------------------------------------------------------------
+
+dotnet tool install --global dotnet-aspnet-codegenerator
+
+# ----------------------------------------------------------------------
+
+dotnet aspnet-codegenerator razorpage `
+    --model FoodCategory `
+    --dataContext NutritionTrackerRazorPages.Data.NutritionTrackerContext `
+    --useDefaultLayout `
+    --relativeFolderPath Pages\FoodCategories `
+    --referenceScriptLibraries `
+    --useSqlite
+
+# ----------------------------------------------------------------------
+    
+dotnet tool install --global dotnet-ef
+
+dotnet ef database drop --force
